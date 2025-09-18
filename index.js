47be2934001148c3ec0ae3a09077ef2bd677553a
RparraTZ <rparratellez@gmail.com>
2025-09-18 00:44:02 -0700

    JS display date and time
    Did the JS code to display current date and time on both inicial cities with updating time every second.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getMexicoData() {
+  let city = document.getElementById("mexico-city");
+  let cityName = city.querySelector(".CityName");
+  let cityDate = city.querySelector(".CityDate");
+  let cityTime = city.querySelector(".CityTime");
+  cityDate.innerHTML = moment()
+    .tz("America/Mexico_City")
+    .format("dddd MMMM Do YYYY");
+  cityTime.innerHTML = moment()
+    .tz("America/Mexico_City")
+    .format("h:mm:ss[<small>] A[</small]");
+}
+function getOsloData() {
+  let city = document.getElementById("oslo");
+  let cityName = city.querySelector(".CityName");
+  let cityDate = city.querySelector(".CityDate");
+  let cityTime = city.querySelector(".CityTime");
+  cityDate.innerHTML = moment().tz("Europe/Oslo").format("dddd MMMM Do YYYY");
+  cityTime.innerHTML = moment()
+    .tz("Europe/Oslo")
+    .format("h:mm:ss[<small>] A[</small]");
+}
 
+setInterval(getMexicoData, 1000);
+setInterval(getOsloData, 1000);
+let citiesSelectionElement = document.getElementById("cities");
+citiesSelectionElement.addEventListener("change", getSelectionData);
